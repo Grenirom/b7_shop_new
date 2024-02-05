2829eef36ce06bf10bf82708fe69d6a8dc78e652
Nick <ngrebnev17@gmail.com>
2024-02-05 16:28:25 +0600

    cors added

--- apps/account/send_email.py
+++ apps/account/send_email.py
@@ -1,6 +1,6 @@
 from decouple import config
 from django.core.mail import send_mail
-
+from django.template.loader import render_to_string
 HOST = config('HOST_FOR_SEND_EMAIL')
 
 
--- config/settings.py
+++ config/settings.py
@@ -308,3 +308,7 @@ LANGUAGES = [
 ]
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
