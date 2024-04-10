77d898d6b6ba389501e0b16f03950be64e78f7b2
Nick <ngrebnev17@gmail.com>
2024-04-10 16:12:43 +0300

    cache for products and categories GET requests

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import generics, permissions
 
 from apps.categories.models import Category
@@ -9,9 +11,16 @@ class CategoryListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny, ]
     serializer_class = CategorySerializer
 
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 
 class CategoryDetailView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     permission_classes = [permissions.AllowAny, ]
     serializer_class = CategorySerializer
 
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
